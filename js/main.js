fdae622be9ba6ca59ec9da6abb7d4b21ca95d796
Frances Cancio <frances.cancio@gmail.com>
2019-08-02 17:23:19 -0500

    Complete initial HTML and CSS and JS const/variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 /*----- constants -----*/ 
-
+const chinLookup = {
+    th: {
+        chinImg: 'imgs/TomHanks.jpg',
+        fullImg: 'imgs/TomHanks.jpg'
+    }
+}
 /*----- app's state (variables) -----*/ 
-
+let score, image
 
 /*----- cached element references -----*/ 
+const scoreEl = document.getElementById()
 
 /*----- event listeners -----*/ 
-
+document.querySelector('main button')
+    .addEventListener('click', checkAnswer)
 
 /*----- functions -----*/
+init();
+
+function init() {
+    score = 0;
+    image = 'imgs/TomHanks.jpg'
+}
+render();
