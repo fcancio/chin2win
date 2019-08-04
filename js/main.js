8b1238ac14ee071747780c34df50b5f11c4a162d
Frances Cancio <frances.cancio@gmail.com>
2019-08-04 14:34:26 -0500

    Checks user input vs correct answer

--- js/main.js
+++ js/main.js
@@ -8,47 +8,45 @@ const chins = [
     {
         chinImg: 'imgs/TomHanks_chin.png',
         fullImg: 'imgs/TomHanks.jpg',
-        answer: 'Tom Hanks'
+        answer: 'tom hanks'
     },
     {
         chinImg: 'imgs/MichaelKeaton_chin.png',
         fullImg: 'imgs/MichaelKeaton.jpg',
-        answer: 'Michael Keaton'
+        answer: 'michael keaton'
     },
 ]
 
 /*----- app's state (variables) -----*/ 
 let score, image
 
+let rndImgIdx = Math.floor(Math.random() * chins.length);
+
 /*----- cached element references -----*/ 
 let photoContainer = document.getElementById('photo')
 let scoreEl = document.getElementsByClassName('counter')
-let playerGuess = document.querySelector('input')
+let userInput = document.querySelector('input')
+let submitAnswer = document.querySelector('button').addEventListener('click', checkGuess)
 
 /*----- event listeners -----*/ 
 
 
 /*----- functions -----*/
-console.log(chins[0].chinImg)
-console.log(chins[0].fullImg)
-console.log(chins[0].answer)
-console.log(chins[1].chinImg)
-console.log(chins[1].fullImg)
-console.log(chins[1].answer)
 
 init();
 
 function init() {
     score = 0;
     image = document.createElement("img")
-    image.setAttribute("src", `${chins[0].chinImg}`)
-    photoContainer.appendChild(image)
+        // let rndImgIdx = Math.floor(Math.random() * chins.length);
+        image.setAttribute("src", `${chins[rndImgIdx].chinImg}`)
+        photoContainer.appendChild(image)
 }
 render();
 
 function render() {
     
-    // let rndImgIdx = Math.floor(Math.random() * chins.length);
+
 
     //render image
 
@@ -58,5 +56,16 @@ function render() {
 }
 
 function checkGuess() {
+// when button is clicked, compare player's input with chins[i].answer
+let chinAnswer = chins[rndImgIdx].answer
+userInput = document.querySelector('input').value.toLowerCase()
+console.log(userInput)
+if(userInput === chinAnswer) {
+    alert('Right!')
+} else {
+    alert('Guess again!')
+}
+
+
 //     if (playerGuess === 
 }
